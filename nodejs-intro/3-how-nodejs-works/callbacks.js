8c75d6c2cee3233bc2deda16719185e8a934062a
Mario Lazzari <mario.lazzari@gmail.com>
2023-11-05 19:28:31 +0100

    first commit

---
+++ nodejs-intro/3-how-nodejs-works/callbacks.js
@@ -0,0 +1,16 @@
+// callback asynchrounous example
+
+const fs = require('fs');
+const filePath = './file.txt';
+
+// request to read a file
+fs.readFile(filePath, 'utf8', (error, data) => {
+    if (error) {
+        console.log('An error occurred...: ', error);
+    } else {
+        console.log(data);
+        console.log('Done!');
+    }
+});
+
+console.log('Im the last line of the file');
