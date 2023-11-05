20a8130ceac8bca8bba018bc7bc215b143b7d997
Mario Lazzari <mario.lazzari@gmail.com>
2023-11-05 21:47:26 +0100

    Done chapter 1

--- nodejs-intro/3-how-nodejs-works/async-await.js
+++ nodejs-intro/3-how-nodejs-works/async-await.js
@@ -4,14 +4,14 @@ const fs = require('fs').promises;
 const filePath = './file.txt';
 
 async function readFileAsync() {
-    try {
-        // request to read a file
-        const data = await fs.readFile(filePath, 'utf8');
-        console.log(data);
-        console.log('Done!');
-    } catch (error) {
-        console.log('An error occurred...: ', error);
-    }
+  try {
+    // request to read a file
+    const data = await fs.readFile(filePath, 'utf8');
+    console.log(data);
+    console.log('Done!');
+  } catch (error) {
+    console.log('An error occurred...: ', error);
+  }
 }
 
 readFileAsync();
